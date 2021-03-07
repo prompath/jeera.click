36e81ce59b17f5755b99f11c77351f0343b84413
Jeerawat Chuachan <jeerawatchu@gmail.com>
2021-03-07 11:12:45 +0700

    Add bootstrap and moment. Add errorhandler. Add Navbar. Add base.html from bootstrap.

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return render_template("index.html")
